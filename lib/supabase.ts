30af49ccdcf2c614b48b3400e26f6bd97682b594
git1316 <akshay060103@gmail.com>
2026-02-18 15:20:56 +0530

    fix supabase client properly

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabasePublishKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabasePublishKey);
+const supabaseKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error("Supabase environment variables are missing");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
